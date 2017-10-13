a35106e9c5df92c6d295ecceca0a0565c0997841
Hiroyuki Sano <sh19910711@gmail.com>
2017-10-14 00:32:22 +0900

    add ruby

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,22 @@
 sudo echo hello isucon
 
+-- mysql
+mkdir -p /tmp/mysql
 cp config/my.cnf /usr/local/etc/my.cnf
 pkill mysqld
 mysqld &
 
+-- nginx
+mkdir -p /tmp/nginx
 cp config/nginx.conf /usr/local/etc/nginx/nginx.conf
 sudo pkill nginx
 sudo nginx &
+
+-- webapp
+pushd ruby
+pkill ruby
+bundle install -j4
+RACK_ENV=production bundle exec rackup --host localhost --port 8080 &
+popd
+
+echo finished
---
+++ ruby/unicorn_config.rb
@@ -0,0 +1,4 @@
+worker_processes 4
+preload_app true
+listen 80
+pid "tmp/unicorn.pid"
