4d6784b2ecbe52efafc3ea2066b6c4b25b86ef67
Hiroyuki Sano <sh19910711@gmail.com>
2017-10-13 23:47:53 +0900

    deploy: reload nginx

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
-cp config/my.cnf /usr/local/etc/my.cnf
+sudo echo hello isucon
 
+cp config/my.cnf /usr/local/etc/my.cnf
 pkill mysqld
 mysqld &
+
+cp config/nginx.conf /usr/local/etc/nginx/nginx.conf
+sudo pkill nginx
+sudo nginx &
